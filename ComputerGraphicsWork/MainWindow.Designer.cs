d3f397373b44eb27f8b57d990b402a754b21dd5d
141242068-ouxianfei <141242068@smail.nju.edu.cn>
2017-04-04 15:24:46 +0800

    first commit

---
+++ ComputerGraphicsWork/MainWindow.Designer.cs
@@ -0,0 +1,218 @@
+﻿using System.Drawing;
+using System.Collections;
+
+
+namespace ComputerGraphicsWork
+{
+    public class CGUserGraphics
+    {
+        public void TagPoints()
+        {
+
+        }
+    }
+
+    public class CGUserGraphicsPoint : CGUserGraphics
+    {
+
+    }
+    public class CGUserGraphicsLine : CGUserGraphics
+    {
+
+    }
+    public class CGUserGraphicsCircle : CGUserGraphics
+    {
+
+    }
+
+    public class CGUserCanvas
+    {
+        private int canvasWidth, canvasHeight;
+        private Bitmap userCanvas;
+        private int[,] refCount;
+
+        private void SetPixel(Point pos)
+        {
+            userCanvas.SetPixel(pos.X, pos.Y, Color.Black);
+            refCount[pos.X, pos.Y]++;
+        }
+
+        private void ClearPixel(Point pos)
+        {
+            refCount[pos.X, pos.Y]--;
+            if(refCount[pos.X, pos.Y] <= 0)
+                userCanvas.SetPixel(pos.X, pos.Y, Color.White);
+        }
+
+        private Point ClipPoint(Point pos)
+        {
+            if (pos.X < 0) pos.X = 0;
+            if (pos.X >= canvasWidth) pos.X = canvasWidth - 1;
+            if (pos.Y < 0) pos.Y = 0;
+            if (pos.Y >= canvasHeight) pos.Y = canvasHeight - 1;
+            return pos;
+        }
+
+        public void Init(int width, int height)
+        {
+            canvasWidth = width;
+            canvasHeight = height;
+            userCanvas = new Bitmap(width, height);
+            refCount = new int[width, height];
+        }
+
+        public void SelectGraphics(CGUserGraphics userGraphics)
+        {
+
+        }
+
+        public void ClearGraphics(CGUserGraphics userGraphics)
+        {
+
+        }
+
+        public void FindGraphics(Point pos)
+        {
+
+        }
+
+    }
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.commandHolder = new System.Windows.Forms.ToolStrip();
+            this.buttonDrawPoint = new System.Windows.Forms.ToolStripButton();
+            this.buttonDrawLine = new System.Windows.Forms.ToolStripButton();
+            this.buttonDrawCircle = new System.Windows.Forms.ToolStripButton();
+            this.buttonDrawEllipse = new System.Windows.Forms.ToolStripButton();
+            this.buttonDrawNothing = new System.Windows.Forms.ToolStripButton();
+            this.commandHolder.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // commandHolder
+            // 
+            this.commandHolder.Dock = System.Windows.Forms.DockStyle.Left;
+            this.commandHolder.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.buttonDrawPoint,
+            this.buttonDrawLine,
+            this.buttonDrawCircle,
+            this.buttonDrawEllipse,
+            this.buttonDrawNothing});
+            this.commandHolder.Location = new System.Drawing.Point(0, 0);
+            this.commandHolder.Name = "commandHolder";
+            this.commandHolder.Size = new System.Drawing.Size(33, 381);
+            this.commandHolder.Stretch = true;
+            this.commandHolder.TabIndex = 0;
+            this.commandHolder.Text = "commandHolder";
+            this.commandHolder.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.commandHolder_ItemClicked);
+            // 
+            // buttonDrawPoint
+            // 
+            this.buttonDrawPoint.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.buttonDrawPoint.Image = ((System.Drawing.Image)(resources.GetObject("buttonDrawPoint.Image")));
+            this.buttonDrawPoint.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.buttonDrawPoint.Name = "buttonDrawPoint";
+            this.buttonDrawPoint.Size = new System.Drawing.Size(30, 20);
+            this.buttonDrawPoint.Text = "buttonDrawPoint";
+            this.buttonDrawPoint.Click += new System.EventHandler(this.buttonDrawPoint_Click);
+            // 
+            // buttonDrawLine
+            // 
+            this.buttonDrawLine.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.buttonDrawLine.Image = ((System.Drawing.Image)(resources.GetObject("buttonDrawLine.Image")));
+            this.buttonDrawLine.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.buttonDrawLine.Name = "buttonDrawLine";
+            this.buttonDrawLine.Size = new System.Drawing.Size(30, 20);
+            this.buttonDrawLine.Text = "buttonDrawLine";
+            this.buttonDrawLine.ToolTipText = "buttonDrawLine";
+            this.buttonDrawLine.Click += new System.EventHandler(this.buttonDrawLine_Click);
+            // 
+            // buttonDrawCircle
+            // 
+            this.buttonDrawCircle.AutoSize = false;
+            this.buttonDrawCircle.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.buttonDrawCircle.Image = ((System.Drawing.Image)(resources.GetObject("buttonDrawCircle.Image")));
+            this.buttonDrawCircle.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.buttonDrawCircle.Name = "buttonDrawCircle";
+            this.buttonDrawCircle.Size = new System.Drawing.Size(32, 32);
+            this.buttonDrawCircle.Text = "buttonDrawCircle";
+            this.buttonDrawCircle.Click += new System.EventHandler(this.buttonDrawCircle_Click);
+            // 
+            // buttonDrawEllipse
+            // 
+            this.buttonDrawEllipse.AutoSize = false;
+            this.buttonDrawEllipse.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.buttonDrawEllipse.Image = ((System.Drawing.Image)(resources.GetObject("buttonDrawEllipse.Image")));
+            this.buttonDrawEllipse.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.buttonDrawEllipse.Name = "buttonDrawEllipse";
+            this.buttonDrawEllipse.Size = new System.Drawing.Size(32, 32);
+            this.buttonDrawEllipse.Text = "buttonDrawEllipse";
+            this.buttonDrawEllipse.ToolTipText = "buttonDrawEllipse";
+            this.buttonDrawEllipse.Click += new System.EventHandler(this.buttonDrawEllipse_Click);
+            // 
+            // buttonDrawNothing
+            // 
+            this.buttonDrawNothing.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.buttonDrawNothing.Image = ((System.Drawing.Image)(resources.GetObject("buttonDrawNothing.Image")));
+            this.buttonDrawNothing.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.buttonDrawNothing.Name = "buttonDrawNothing";
+            this.buttonDrawNothing.Size = new System.Drawing.Size(30, 20);
+            this.buttonDrawNothing.Text = "buttonDrawNothing";
+            this.buttonDrawNothing.Click += new System.EventHandler(this.buttonDrawNothing_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(635, 381);
+            this.Controls.Add(this.commandHolder);
+            this.Name = "MainWindow";
+            this.Text = "MainWindow";
+            this.Load += new System.EventHandler(this.MainWindow_OverideLoad);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainWindow_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainWindow_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MainWindow_MouseUp);
+            this.commandHolder.ResumeLayout(false);
+            this.commandHolder.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip commandHolder;
+        private System.Windows.Forms.ToolStripButton buttonDrawLine;
+        private System.Windows.Forms.ToolStripButton buttonDrawCircle;
+        private System.Windows.Forms.ToolStripButton buttonDrawEllipse;
+        private System.Windows.Forms.ToolStripButton buttonDrawNothing;
+        private System.Windows.Forms.ToolStripButton buttonDrawPoint;
+    }
+}
---
+++ ComputerGraphicsWork/MainWindow.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace ComputerGraphicsWork
+{
+    enum CGGraphicsType
+    {
+        CGTypeNothing = 0,
+        CGTypePoint = 1,
+        CGTypeLine = 2,
+        CGTypeCircle = 3,
+        CGTypeEllipse = 4,
+    };
+
+    enum CGMouseState
+    {
+        CGMouseStateDown = 0,
+        CGMouseStateUp = 1,
+        CGMouseStateTwiceDown = 2,
+        CGMouseStateTwiceUp = 3,
+    };
+
+    public partial class MainWindow : Form
+    {
+        private CGGraphicsType ghsType;
+        private CGMouseState mouseState;
+        private Graphics ghs;
+        private Point oldPos, curPos, downPos;
+        private Pen ghsPen = new Pen(Color.Black, 1);
+        private Pen rawPen = new Pen(Color.White, 1);
+        private bool canDrawGraphics = false;
+        private Bitmap userCanvas;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void MainWindow_OverideLoad(object sender, EventArgs e)
+        {
+            ghs = this.CreateGraphics();
+            userCanvas = new Bitmap(this.ClientRectangle.Width, this.ClientRectangle.Height);
+        }
+
+        private void ResumeOldStripButton(CGGraphicsType gt)
+        {
+            switch (gt)
+            {
+                case CGGraphicsType.CGTypePoint:
+                    this.buttonDrawPoint.Checked = false;
+                    break;
+                case CGGraphicsType.CGTypeLine:
+                    this.buttonDrawLine.Checked = false;
+                    break;
+                case CGGraphicsType.CGTypeCircle:
+                    this.buttonDrawCircle.Checked = false;
+                    break;
+                case CGGraphicsType.CGTypeEllipse:
+                    this.buttonDrawEllipse.Checked = false;
+                    break;
+            }
+        }
+
+        private void buttonDrawPoint_Click(object sender, EventArgs e)
+        {
+            if (ghsType == CGGraphicsType.CGTypePoint)
+            {
+                this.buttonDrawPoint.Checked = false;
+                ghsType = CGGraphicsType.CGTypeNothing;
+            }
+            else
+            {
+                ResumeOldStripButton(ghsType);
+                this.buttonDrawPoint.Checked = true;
+                ghsType = CGGraphicsType.CGTypePoint;
+            }
+        }
+
+        private void buttonDrawLine_Click(object sender, EventArgs e)
+        {
+            if(ghsType == CGGraphicsType.CGTypeLine)
+            {
+                this.buttonDrawLine.Checked = false;
+                ghsType = CGGraphicsType.CGTypeNothing;
+            }
+            else
+            {
+                ResumeOldStripButton(ghsType);
+                this.buttonDrawLine.Checked = true;
+                ghsType = CGGraphicsType.CGTypeLine;
+            }
+        }
+
+        private void buttonDrawCircle_Click(object sender, EventArgs e)
+        {
+            if (ghsType == CGGraphicsType.CGTypeCircle)
+            {
+                this.buttonDrawCircle.Checked = false;
+                ghsType = CGGraphicsType.CGTypeNothing;
+            }
+            else
+            {
+                ResumeOldStripButton(ghsType);
+                this.buttonDrawCircle.Checked = true;
+                ghsType = CGGraphicsType.CGTypeCircle;
+            }
+        }
+
+        private void buttonDrawEllipse_Click(object sender, EventArgs e)
+        {
+            if (ghsType == CGGraphicsType.CGTypeEllipse)
+            {
+                this.buttonDrawEllipse.Checked = false;
+                ghsType = CGGraphicsType.CGTypeNothing;
+            }
+            else
+            {
+                ResumeOldStripButton(ghsType);
+                this.buttonDrawEllipse.Checked = true;
+                ghsType = CGGraphicsType.CGTypeEllipse;
+            }
+        }
+
+        private void buttonDrawNothing_Click(object sender, EventArgs e)
+        {
+            if (ghsType == CGGraphicsType.CGTypeNothing)
+            {
+                this.buttonDrawNothing.Checked = false;
+                ghsType = CGGraphicsType.CGTypeNothing;
+            }
+            else
+            {
+                ResumeOldStripButton(ghsType);
+                this.buttonDrawNothing.Checked = true;
+                ghsType = CGGraphicsType.CGTypeNothing;
+            }
+        }
+
+        private void MainWindow_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseState = CGMouseState.CGMouseStateUp;
+            canDrawGraphics = false;
+        }
+
+        private void MainWindow_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseState = CGMouseState.CGMouseStateDown;
+            canDrawGraphics = true;
+            downPos.X = e.X;
+            downPos.Y = e.Y;
+        }
+
+
+        private void commandHolder_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void wrapDrawCircle(Pen pen, Point center, Point edge)
+        {
+            int dx = edge.X - center.X;
+            int dy = edge.Y - center.Y;
+            double cx = center.X;
+            double cy = center.Y;
+            double r = Math.Sqrt((dx * dx + dy * dy));
+            ghs.DrawEllipse(pen, new RectangleF((float)(cx - r), (float)(cy - r), (float)(2 * r), (float)(2 * r)));
+
+        }
+        private void wrapDrawEllipse(Pen pen, Point center, Point edge)
+        {
+            int dx = Math.Abs(edge.X - center.X);
+            int dy = Math.Abs(edge.Y - center.Y);
+            int cx = center.X;
+            int cy = center.Y;
+            double e = Math.Sqrt((dx * dx + dy * dy) / 2);
+            ghs.DrawEllipse(pen, new RectangleF((float)(cx - dx), (float)(cy - dy), (float)(2 * dx), (float)(2 * dy)));
+
+        }
+        private void MainWindow_MouseMove(object sender, MouseEventArgs e)
+        {
+            oldPos = curPos;
+            curPos.X = e.X;
+            curPos.Y = e.Y;
+
+            if (!canDrawGraphics || mouseState == CGMouseState.CGMouseStateUp)
+            {
+                return;
+            }
+
+            switch (ghsType)
+            {
+                case CGGraphicsType.CGTypePoint:
+                    userCanvas.SetPixel(curPos.X, curPos.Y, Color.Black);
+                    ghs.DrawImage(userCanvas, new Rectangle(0, 0, this.ClientRectangle.Width, this.ClientRectangle.Height));
+                    break;
+                case CGGraphicsType.CGTypeLine:
+                    ghs.DrawLine(rawPen, downPos, oldPos);
+                    ghs.DrawLine(ghsPen, downPos, curPos);
+                    break;
+                case CGGraphicsType.CGTypeCircle:
+                    wrapDrawCircle(rawPen, downPos, oldPos);
+                    wrapDrawCircle(ghsPen, downPos, curPos);
+                    break;
+                case CGGraphicsType.CGTypeEllipse:
+                    wrapDrawEllipse(rawPen, downPos, oldPos);
+                    wrapDrawEllipse(ghsPen, downPos, curPos);
+                    break;
+            }
+        }
+    }
+}
